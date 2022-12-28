1f9833f1504faabb2ded6c792e1bf28f12319acc
HaroldZQ <hazq0306@gmail.com>
2022-12-28 05:34:52 -0500

    JuegoCompleto1

--- juegoSokobanFinaly.py
+++ juegoSokobanFinaly.py
@@ -3,11 +3,11 @@ import numpy as np
 
 pygame.init()
 tablero = np.array([[0,0,0,0,0,0,0,0]   #tablero [fila][columna]->inicio: tablero[0][0]
-                   ,[0,0,0,0,1,1,0,0]   #En 0 se encuentran las cajas
-                   ,[0,0,1,1,2,1,0,0]   #En 1,2,3 y 4 se desplaza el personaje
-                   ,[0,0,1,0,1,0,0,0]   #En 2,3,4 se encuentran los bloques movibles
-                   ,[0,1,1,0,1,0,5,0]
-                   ,[0,1,0,1,1,2,5,0]
+                   ,[0,0,0,0,1,6,0,0]   #En 0 se encuentran las cajas
+                   ,[0,0,1,1,2,1,0,0]   #En >=1 se desplaza el personaje
+                   ,[0,0,1,0,1,0,0,0]   #En 2 se encuentran los bloques movibles
+                   ,[0,1,1,0,1,0,5,0]   #En 5 se encuentran los diamantes
+                   ,[0,1,0,1,1,2,5,0]   #En 6 se encuentran el personaje
                    ,[0,1,2,1,1,1,5,0]
                    ,[0,0,0,0,0,0,0,0]])
 
@@ -67,11 +67,8 @@ Diamante = pygame.transform.scale(Diamante, (50,50))
 #VARIABLES
 #-------------------------------------------------
 #Definimos las coordenadas iniciales del personaje
-cord_x = 465
-cord_y = 125
-#cajas Movibles:
-cordC_x = 405
-cordC_y = 185 
+x = 5
+y = 1
 #Velocidad de personaje
 x_speed = 0
 y_speed = 0
@@ -88,31 +85,6 @@ def mapaJuego():
                 y_1 = 65+60*y
                 screen.blit(bloque,[x_1,y_1,50,50])
                 
-
-#Funcion de mapa movilidad-EN PRUEBA
-def pos_inicial():
-    for x in range(0,8): #x:columna
-        for y in range(0,8): #y:fila
-            if tablero[y][x]>=1: #tablero[fila][columna]
-                #cord_x = 465  
-                #cord_y = 125
-                screen.blit(personaje,[cord_x,cord_y,50,50])
-
-#[1]Funcion de mapa movilidad-EN PRUEBA-BUSCAR SOLUCIÓN
-def mov_personaje(cordx,cordy,xspeed,yspeed):
-    if (165<=cordx and cordx<=585)and(65<=cordy and cordy<=485): 
-        cordx += xspeed
-        cordy += yspeed
-        x = int((cordx-165)/60)
-        y = int((cordy-65)/60)
-        print(x,y)
-        if tablero[y][x]>=1:
-            screen.blit(personaje,[cordx,cordy,50,50])
-        else:
-            cordx -= xspeed
-            cordy -= yspeed
-            screen.blit(personaje,[cordx,cordy,50,50])
-
 #Función para los bloques movibles
 def bloqueMovible():
     for x in range(0,8): #x:columna
@@ -121,9 +93,16 @@ def bloqueMovible():
                 x_1 = 165+60*x
                 y_1 = 65+60*y
                 screen.blit(bloque_Mov,[x_1,y_1,50,50])
-#------------------------------------------------------    
+#Función para jugador
+def jugadorMovible():
+    for x in range(0,8): #x:columna
+        for y in range(0,8): #y:fila
+            if tablero[y][x]==6: #tablero[fila][columna]
+                x_1 = 165+60*x
+                y_1 = 65+60*y
+                screen.blit(personaje,[x_1,y_1,50,50]) 
 
-#Función para los bloques movibles
+#Función para los diamantes
 def diamante():
     for x in range(0,8): #x:columna
         for y in range(0,8): #y:fila
@@ -131,6 +110,7 @@ def diamante():
                 x_1 = 165+60*x
                 y_1 = 65+60*y
                 screen.blit(Diamante,[x_1,y_1,50,50])
+#------------------------------------------------------  
 #REALIZA JUEGO-BULCE INFINITO
 while True:
     for event in pygame.event.get():
@@ -139,95 +119,159 @@ while True:
          #Evento teclado
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x_speed = -60
-                #izquierda = True
+                x_speed = -1               
 
             if event.key == pygame.K_RIGHT:
-                x_speed =60
-                #derecha = True
+                x_speed =1
 
             if event.key == pygame.K_UP:
-                y_speed = -60
-                #arriba = True
+                y_speed = -1
 
             if event.key == pygame.K_DOWN:
-                y_speed = 60
-                #abajo = True
+                y_speed = 1
             
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 x_speed = 0
-                #izquierda = False
+
             if event.key == pygame.K_RIGHT:
                 x_speed = 0
-                #derecha = False
+
             if event.key == pygame.K_UP:
                 y_speed = 0
-                #arriba = False
+
             if event.key == pygame.K_DOWN:
                 y_speed = 0
-                #abajo = False
 
 
     #Color de fondo
     screen.blit(background,[-10,-10])
     
     mapaJuego() 
-    #bloqueMovible()
-    #Para mover personaje-CONVERTIRLO EN UNA FUNCIÓN[1]
-    if (165<=cord_x and cord_x<=585)and(65<=cord_y and cord_y<=485):
-        cord_x += x_speed
-        cord_y += y_speed
-        x = int((cord_x-165)/60)
-        y = int((cord_y-65)/60)
-        print(x,y) #Para ver coordenada de la M.Bidimensional
-        if tablero[y][x]>=1 :
-            print(tablero)
-            screen.blit(personaje,[cord_x,cord_y,50,50])
-            if tablero[y][x]==2:
-                #cordC_x += x_speed #Izquierda->cordC_x=405-60
-                #cordC_y += y_speed #Izquierda->cordC_y=185
-                #screen.blit(bloque_Mov,[cord_x+x_speed,cordC_y+y_speed,50,50])
-                #screen.blit(personaje,[cord_x,cord_y,50,50])->No es necesario colocarlo aqui
-                if x_speed == -60: #Izquirda
-                    if tablero[y][x-1]!=0:
-                        tablero[y][x]=1
+    #Para mover personaje,cajas
+    x += x_speed
+    y += y_speed
+    if x_speed == -1: #Izquierda
+        if tablero[y][x]!=0:
+            if tablero[y][x]==1 or tablero[y][x]==5: #Para mover por 1y5(Los diamantes)
+                tablero[y][x]=6
+                tablero[y][x+1]=1
+            elif tablero[y][x]==2: #Para mover bloque(Estamos en 2)
+                if tablero[y][x-1]!=0:
+                    if tablero[y][x-1]!=2:
                         tablero[y][x-1]=2
-                    
-                elif y_speed == 60: #Abajo
-                    if tablero[y+1][x]!=0:
-                        tablero[y][x]=1
+                        tablero[y][x]=6
+                        tablero[y][x+1]=1
+                    else:#Caso,cuando se chocan 2 bloques movibles
+                        tablero[y][x]=2
+                        tablero[y][x+1]=6
+                        tablero[y][x-1]=2 
+                        x=x+1
+
+                else:
+                    tablero[y][x]=2
+                    tablero[y][x+1]=6
+                    x =x+1
+                    #O tambien podemos colocar:
+                    #x -= x_speed
+                    #y -= y_speed
+        else:
+            x -= x_speed
+            y -= y_speed
+                                 
+    elif y_speed == 1: #Abajo
+        if tablero[y][x]!=0:
+            if tablero[y][x]==1 or tablero[y][x]==5: #Para mover por 1y5(Los diamantes)
+                tablero[y][x]=6
+                tablero[y-1][x]=1
+            elif tablero[y][x]==2: #Para mover bloque(Estamos en 2)
+                if tablero[y+1][x]!=0:
+                    if tablero[y+1][x]!=2:
                         tablero[y+1][x]=2
-                elif y_speed == -60: #Arriba
-                    if tablero[y-1][x]!=0:
-                        tablero[y][x]=1
+                        tablero[y][x]=6
+                        tablero[y-1][x]=1
+                    else:#Caso,cuando se chocan 2 bloques movibles
+                        tablero[y][x]=2
+                        tablero[y-1][x]=6
+                        tablero[y+1][x]=2
+                        y=y-1
+
+                else:
+                    tablero[y][x]=2
+                    tablero[y-1][x]=6
+                    y =y-1
+                    #O tambien podemos colocar:
+                    #x -= x_speed
+                    #y -= y_speed
+        else:
+            x -= x_speed
+            y -= y_speed
+        
+    elif y_speed == -1: #Arriba
+        if tablero[y][x]!=0:
+            if tablero[y][x]==1 or tablero[y][x]==5: #Para mover por 1y5(Los diamantes)
+                tablero[y][x]=6
+                tablero[y+1][x]=1
+            elif tablero[y][x]==2: #Para mover bloque(Estamos en 2)
+                if tablero[y-1][x]!=0:
+                    if tablero[y-1][x]!=2:
                         tablero[y-1][x]=2
-                elif x_speed == 60: #Derecha
-                    if tablero[y][x+1]!=0:
-                        tablero[y][x]=1
+                        tablero[y][x]=6
+                        tablero[y+1][x]=1
+                    else:#Caso,cuando se chocan 2 bloques movibles
+                        tablero[y][x]=2
+                        tablero[y+1][x]=6
+                        tablero[y-1][x]=2
+                        y=y+1
+
+                else:
+                    tablero[y][x]=2
+                    tablero[y+1][x]=6
+                    y =y+1
+                    #O tambien podemos colocar:
+                    #x -= x_speed
+                    #y -= y_speed
+        else:
+            x -= x_speed
+            y -= y_speed
+        
+    elif x_speed == 1: #Derecha
+        if tablero[y][x]!=0:
+            if tablero[y][x]==1 or tablero[y][x]==5: #Para mover por 1y5(Los diamantes)
+                tablero[y][x]=6
+                tablero[y][x-1]=1
+            elif tablero[y][x]==2: #Para mover bloque(Estamos en 2)
+                if tablero[y][x+1]!=0:
+                    if tablero[y][x+1]!=2:
                         tablero[y][x+1]=2
-               
-        elif tablero[y][x]==0:
-            cord_x -= x_speed
-            cord_y -= y_speed
-            print(tablero)
-            screen.blit(personaje,[cord_x,cord_y,50,50])
-            
-            #if x_speed == -60: #Izquierda
-            #    if tablero[y][x+1]==2:
-            #        tablero[y][x]=0
-            #        x_1 = 165+60*(x+1)
-            #        y_1 = 65+60*y
-            #        screen.blit(Diamante,[x_1,y_1,50,50])
-                
+                        tablero[y][x]=6
+                        tablero[y][x-1]=1 
+                    else:#Caso,cuando se chocan 2 bloques movibles
+                        tablero[y][x]=2
+                        tablero[y][x-1]=6
+                        tablero[y][x+1]=2 
+                        x=x-1
+                else:
+                    tablero[y][x]=2
+                    tablero[y][x-1]=6
+                    x =x-1
+                    #O tambien podemos colocar:
+                    #x -= x_speed
+                    #y -= y_speed                    
+        else:
+            x -= x_speed
+            y -= y_speed
+    #Para monitoriar juego:
+    print(tablero)
+    print(x,y)
             #Colocando sonido, cuando el personaje choque en el muro
             #pygame.mixer.music.load("Música y Sonido/SaltosoMovi.mpeg")
             #pygame.mixer.music.play()
             
                        
     #mapaJuego() #Llamamos función del mapa
-    #mov_personaje(cord_x,cord_y,x_speed,y_speed)
-    
+    #Jugador Movible
+    jugadorMovible()
     #Bloques movibles      
     bloqueMovible()
 
