e72846a76759449507b100d0e8e12880be5640b9
daniel-enrique1 <121458856+daniel-enrique1@users.noreply.github.com>
2022-12-28 23:16:22 -0500

    Add files via upload

---
+++ button.py
@@ -0,0 +1,30 @@
+import pygame
+
+#button class
+class Button():
+	def __init__(self, x, y, image, scale):
+		width = image.get_width()
+		height = image.get_height()
+		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False
+
+	def draw(self, surface):
+		action = False
+		#Obtener la posición del mouse
+		pos = pygame.mouse.get_pos()
+
+		#verifique las condiciones de paso del mouse y de realizar el clic
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		#Dibujar botón en pantalla
+		surface.blit(self.image, (self.rect.x, self.rect.y))
+
+		return action
